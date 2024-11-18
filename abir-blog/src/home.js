bec993018efafd557ef3cc3f76cbe8f0a2bb111d
Abir Hasan <abirsonet563@gmail.com>
2024-11-18 10:23:57 +0000

    View Blogs Details on Home Page

--- abir-blog/src/home.js
+++ abir-blog/src/home.js
@@ -1,22 +1,23 @@
 import {useState} from 'react';
 const Home = () => {
-    // let name = "Abir Hasan"
-const [name, setName] = useState('Abir');
-    const ClickAgain = () =>{
-          setName('Abir Hasan');
-    }
-const [age, setAge] = useState('None')
-const ageTwo = () => {
-    setAge(23);
-    console.log(setAge);
-}
+const [blogs, setBlogs] = useState([
+{title: 'My new web', body: 'lorem ipsum...', author: 'abir', id:1},
+{title: 'Welcome Party!', body: 'lorem ipsum...', author: 'hasan', id:2},
+{title: 'Web dev top topic', body: 'lorem ipsum...', author: 'yoshi', id:3},
+{title: 'Earn Money', body: 'lorem ipsum...', author: 'mario', id:4}
+]);
+
+
 return(
     <div className="Home">
    <h2>Home Page</h2>
-   <p>{name}</p>
-   <p>{age}</p>
-   <button onClick={ageTwo}>Age</button>
-   <button onClick={ClickAgain}>Click</button>
+   {blogs.map((blog) => (
+    <div className='blog-preview' key={blog.id}>
+    <h2>Blog Title{blog.title}</h2>    
+    <p>Written by {blog.author}</p>
+    
+    </div>
+   ))}
     </div>
 )
 }
