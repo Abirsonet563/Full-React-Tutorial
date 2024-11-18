3bf1d18c94ed0dfe5e9e676d7562421f38051b3f
Abir Hasan <abirsonet563@gmail.com>
2024-11-18 21:16:04 +0000

    Fetching Data with useEffect

--- abir-blog/src/home.js
+++ abir-blog/src/home.js
@@ -1,26 +1,23 @@
 import {useState, useEffect} from 'react';
 import BlogList from './blogList';
 const Home = () => {
-const [blogs, setBlogs] = useState([
-{title: 'My new web', body: 'lorem ipsum...', author: 'abir', id:1},
-{title: 'Welcome Party!', body: 'lorem ipsum...', author: 'hasan', id:2},
-{title: 'Web dev top topic', body: 'lorem ipsum...', author: 'yoshi', id:3},
-{title: 'Earn Money', body: 'lorem ipsum...', author: 'mario', id:4}
-]);
+const [blogs, setBlogs] = useState(null);
 
 const [name, setName] = useState('Abir');
-const handleDelete = (id) => {
-   const newBlogs = blogs.filter(blog=> blog.id !== id )
-   setBlogs(newBlogs);
-}
+
 useEffect(()=>{
-    console.log('This effect runs in every render');
-    console.log(blogs);
-}, [name])
+   fetch('http://localhost:8000/blogs')
+   .then(res => {
+    return res.json();
+   })
+   .then(data =>{
+     setBlogs(data);
+   });
+}, [])
 return(
     <div className="home">
- <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
- <BlogList blogs={blogs.filter((blog)=>blog.author === "abir")} title="Abir Blogs"/> 
+ {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+ {blogs && <BlogList blogs={blogs.filter((blog)=>blog.author === "abir")} title="Abir Blogs"/> }
   <button onClick={()=>setName('Abir Hasan')}>Abir Click</button>  
   <h1>{name}</h1>
     </div>
