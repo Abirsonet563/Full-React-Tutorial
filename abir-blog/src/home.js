49b061f266f306f962a3b20b73c3daaf5d3d8a14
Abir Hasan <abirsonet563@gmail.com>
2024-11-18 21:21:51 +0000

    Conditional Loading Message

--- abir-blog/src/home.js
+++ abir-blog/src/home.js
@@ -2,20 +2,24 @@ import {useState, useEffect} from 'react';
 import BlogList from './blogList';
 const Home = () => {
 const [blogs, setBlogs] = useState(null);
-
+const [isPending, setIsPending] = useState(true);
 const [name, setName] = useState('Abir');
 
 useEffect(()=>{
-   fetch('http://localhost:8000/blogs')
-   .then(res => {
-    return res.json();
-   })
-   .then(data =>{
-     setBlogs(data);
-   });
+  setTimeout(() => {
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+     return res.json();
+    })
+    .then(data =>{
+      setBlogs(data);
+      setIsPending(false);
+    });
+  }, 1000);
 }, [])
 return(
     <div className="home">
+  {isPending && <div>Loading....</div>}      
  {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
  {blogs && <BlogList blogs={blogs.filter((blog)=>blog.author === "abir")} title="Abir Blogs"/> }
   <button onClick={()=>setName('Abir Hasan')}>Abir Click</button>  
