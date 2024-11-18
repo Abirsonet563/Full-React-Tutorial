e850723d977f999e7d7e152a67c0e960f115535a
Abir Hasan <abirsonet563@gmail.com>
2024-11-18 08:33:03 +0000

    Implement Css and Other Internal Style

--- abir-blog/src/App.js
+++ abir-blog/src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
 
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-       <h1>{title}</h1>
-       <p>Liked {likes} times</p>  
-       <p>{Math.random() * 100}</p>
-       <a href={link}>Google Site</a>
+     <Home/>
       </div>
     </div>
   );
---
+++ abir-blog/src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+
+    return(
+        <nav className="navbar">
+         <h1>The Abir Blog</h1>
+         <div className="links">
+              <a href="/">Home</a>
+              <a href="/create" style={{
+                color: "white",
+                backgroundColor: '#f1356d',
+                borderRadious: '8px'
+              }}>New Blog</a>
+         </div>
+        </nav>
+    )
+}
+export default Navbar;
