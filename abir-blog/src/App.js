463d99a3865967a69f8d40784332b5bf0600e153
Abir Hasan <abirsonet563@gmail.com>
2024-11-18 09:16:03 +0000

    Click Event

--- abir-blog/src/App.js
+++ abir-blog/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './home';
 
+
 function App() {
   return (
     <div className="App">
--- abir-blog/src/home.js
+++ abir-blog/src/home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hi',e);
+      }
+    const handleClickAgain = (name, e) => {
+        console.log(`Hi ${name}`, e.target)
+    }  
 return(
     <div className="Home">
    <h2>Home Page</h2>
+   <button onClick={handleClick}>Click</button>
+   <button onClick={(e)=>
+    handleClickAgain('Abir', e)}>Click Me</button>
     </div>
 )
 }
