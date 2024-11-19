6c605f96460d36f230a5b810a2d6beaffe53321f
Abir Hasan <abirsonet563@gmail.com>
2024-11-19 20:08:31 +0000

    Update href to Link

--- abir-blog/src/Navbar.js
+++ abir-blog/src/Navbar.js
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Navbar = () => {
 
     return(
         <nav className="navbar">
          <h1>The Abir Blog</h1>
          <div className="links">
-              <a href="/">Home</a>
-              <a href="/create" style={{
+              <Link to="/">Home</Link>
+              <Link to="/create" style={{
                 color: "white",
                 backgroundColor: '#f1356d',
                 borderRadious: '8px'
-              }}>New Blog</a>
+              }}>New Blog</Link>
          </div>
         </nav>
     )
